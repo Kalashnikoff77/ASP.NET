5e960f36c13815e70e5843a323f59ade782609d7
okalashnikov <79265352331@yandex.ru>
2025-09-05 11:24:15 +0300

    Тест коммита.

--- Homeworks/Base/src/PromoCodeFactory.WebHost/Startup.cs
+++ Homeworks/Base/src/PromoCodeFactory.WebHost/Startup.cs
@@ -14,10 +14,8 @@ namespace PromoCodeFactory.WebHost
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton(typeof(IRepository<Employee>), (x) => 
-                new InMemoryRepository<Employee>(FakeDataFactory.Employees));
-            services.AddSingleton(typeof(IRepository<Role>), (x) => 
-                new InMemoryRepository<Role>(FakeDataFactory.Roles));
+            services.AddSingleton(typeof(IRepository<Employee>), (x) => new InMemoryRepository<Employee>(FakeDataFactory.Employees));
+            services.AddSingleton(typeof(IRepository<Role>), (x) => new InMemoryRepository<Role>(FakeDataFactory.Roles));
 
             services.AddOpenApiDocument(options =>
             {
